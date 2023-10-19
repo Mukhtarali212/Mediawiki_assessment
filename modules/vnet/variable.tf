40fbdd3366584960c2f8c78e31ff347fa5013696
Mukhtarali212 <49085907+Mukhtarali212@users.noreply.github.com>
2023-10-19 19:22:23 +0530

    Update variable.tf

---
+++ modules/vnet/variable.tf
@@ -0,0 +1,25 @@
+variable "location" {
+  type        = string
+  default     = "eastus2"
+  
+ }
+
+variable "rg_Name" {
+  type        = string
+  default     = ""
+  d
+}
+
+variable "vnet_Name" {
+  type        = string
+  default     = ""
+  description = "Name of the vnet to create "
+  
+}
+variable "vnet_Address" {
+  type        = string
+  default     = ""
+
+}
+
+
